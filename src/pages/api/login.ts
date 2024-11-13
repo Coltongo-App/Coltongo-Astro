487f53a1d8bb0dc8fa6f774fcef4de3e50ed07cf
Diego Valdez <dgogo9@hotmail.com>
2024-11-12 20:44:39 -0600

    Add login and logout logic

---
+++ src/pages/api/login.ts
@@ -0,0 +1,24 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request, redirect, cookies }) => {
+  const API_URL = import.meta.env.API_URL;
+  const formData = await request.formData();
+  const username = formData.get("userField");
+  const password = formData.get("passwordField");
+
+  const response = await fetch(`${API_URL}/user/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+
+  const data = await response.json();
+
+  const { token, userId, expires } = data;
+
+  cookies.set("access-token", token, {
+    path: "/",
+  });
+
+  return redirect("/");
+};
