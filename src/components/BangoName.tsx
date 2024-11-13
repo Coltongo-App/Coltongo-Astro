fb239d836253cadef00a9ae33d9e013a4a8fef0c
Sharon Arana <shar.mdza@gmail.com>
2024-11-12 19:44:38 -0600

    Add Bango name component

--- src/components/BangoName.tsx
+++ src/components/BangoName.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function BangoName() {
-  return <h1>BANGO</h1>;
+const sizes = {
+  large: "text-5xl",
+  small: "text-xl",
+};
+
+type BangoNameProps = {
+  size: "large" | "small";
+};
+
+export default function BangoName({ size }: BangoNameProps) {
+  const fontSize = sizes[size];
+
+  return <h1 className={`${fontSize} font-bango text-vistaWhite-50`}>BANGO</h1>;
 }
